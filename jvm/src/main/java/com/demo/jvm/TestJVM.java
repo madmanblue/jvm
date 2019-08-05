a6008723f83108d1c3e7f5dd997d47577d5fa0d4
blue <1327128544@qq.com>
2019-08-05 23:40:42 +0800

    新增jvm说明

--- jvm/src/main/java/com/demo/jvm/TestJVM.java
+++ jvm/src/main/java/com/demo/jvm/TestJVM.java
@@ -1,13 +1,25 @@
 package com.demo.jvm;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 对于静态字段来说，只有直接定义了该字段的类才会被初始化
  *
  */
 public class TestJVM {
 
-    public static void main(String[] args) {
-        System.out.println(MyChild1.str2);
+    public static void main(String[] args) throws InterruptedException {
+//        System.out.println(MyChild1.str2);
+//        List<MyChild1> list = new ArrayList<>();
+//        while (true) {
+//            list.add(new MyChild1());
+//            Thread.sleep(1000);
+//        }
+        String s1 = "good";
+        String s2 = "good";
+        System.out.println(s1 == s2);
+        System.out.println(s1.equals(s2));
     }
 }
 
